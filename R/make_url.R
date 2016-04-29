d47b928439966b289ee3dfc1f2cdf1f6e325efcc
Steve Simpson <steven.troy.simpson@gmail.com>
2016-04-29 23:59:27 +0000

    version 0.0.3

--- R/make_url.R
+++ R/make_url.R
@@ -5,10 +5,11 @@
 #' @param form Can be either the form_url or form_id
 #' @param do Can be either "get" or "post"
 #' @export
-#' @importFrom magrittr %>%
-#' @examples \dontrun{
-#' url %>% make_url("get") -> get_url
-#' url %>% make_url("post") -> post_url
+#' @include get_form_id.R
+#' @examples
+#' \dontrun{
+#'   url %>% make_url("get") -> get_url
+#'   url %>% make_url("post") -> post_url
 #' }
 make_url <- function (form, do) {
     if (grepl("^https://docs.google.com", form)) {
@@ -16,8 +17,8 @@ make_url <- function (form, do) {
     }
     switch(tolower(do),
            "get"  = httr::modify_url("https://docs.google.com/",
-                                    path = paste0("forms/d/", form, "/viewform")),
+                                    path = paste0("forms/d/", form, "/viewform") ),
            "post" = httr::modify_url("https://docs.google.com/",
-                                     path = paste0("forms/d/", form, "/formResponse")))
+                                     path = paste0("forms/d/", form, "/formResponse") ) )
 }
 
